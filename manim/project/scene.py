1e0e7f614249b52acaebd8fe214a5094dc3bdc98
u7381813 <u7381813@anu.edu.au>
2023-10-21 16:54:27 +1100

    Solving the issue - done

--- manim/project/scene.py
+++ manim/project/scene.py
@@ -5,10 +5,21 @@ from manim import *
 
 class CameraIssue(MovingCameraScene):
     def construct(self):
-        square = Square()
-        triangle = Triangle()
-        square.set_fill(RED, opacity=0.5)
-        self.camera.frame.height = 4
-        self.camera.frame.aspect_ratio = 1
-        self.add(square)
-        self.play(self.camera.frame.animate.rotate(PI / 2, axis=[1.0, 3.0, 1.0]))
+        s1 = Square()
+        s2 = Square()
+        s1.set_fill(RED, opacity=0.5).shift(2 * LEFT)
+        s2.set_fill(GREEN, opacity=0.5).shift(2 * RIGHT)
+        self.add(s1)
+        self.add(s2)
+        self.play(self.camera.frame.animate.rotate(PI / 2))
+
+
+class CameraSolution(MovingCameraScene):
+    def construct(self):
+        s1 = Square()
+        s2 = Square()
+        s1.set_fill(RED, opacity=0.5)
+        s2.set_fill(GREEN, opacity=0.5)
+        group = Group(s1, s2).set_x(0).arrange(buff=1.0)
+        self.add(group)
+        self.play(group.animate.rotate(PI / 2))
