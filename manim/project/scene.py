b4319b1a190251a3808f94f4b798f2bfac541617
u7381813 <u7381813@anu.edu.au>
2023-10-21 23:38:48 +1100

    Reformatting the solution to fit with guidelines

--- manim/project/scene.py
+++ manim/project/scene.py
@@ -18,8 +18,16 @@ class CameraSolution(MovingCameraScene):
     def construct(self):
         s1 = Square()
         s2 = Square()
-        s1.set_fill(RED, opacity=0.5)
-        s2.set_fill(GREEN, opacity=0.5)
-        group = Group(s1, s2).set_x(0).arrange(buff=1.0)
+        s1.set_fill(RED, opacity=0.5).shift(2 * LEFT)
+        s2.set_fill(GREEN, opacity=0.5).shift(2 * RIGHT)
+        self.add(s1)
+        self.add(s2)
+        self.rotate_frame(PI / 2)
+
+    def rotate_frame(self, angle):
+        all_objs = [
+            obj for obj in self.mobjects if not isinstance(obj, ScreenRectangle)
+        ]
+        group = Group(*all_objs).set_x(0).arrange(buff=1.0)
         self.add(group)
-        self.play(group.animate.rotate(PI / 2))
+        self.play(group.animate.rotate(angle))
